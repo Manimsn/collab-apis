95d93616579a1255a7f198778f0cda66be8000e3
Manishmsn <mk065005@gmail.com>
2025-02-01 22:30:24 +0530

    feat(auth): Implement JWT access token verification middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const verifyAccessToken = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Access token missing or invalid" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = decoded?.UserInfo; // Attach user info to request
+    console.log("decoded", decoded);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
+
+export default verifyAccessToken;
