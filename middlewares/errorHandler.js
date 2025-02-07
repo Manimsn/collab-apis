9bfba3b2b78cfcdb0d1fb0891459e84356661f32
Manishmsn <mk065005@gmail.com>
2025-02-07 17:48:07 +0530

    refactor(error-handling): improved error response for handlng invalid json response

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,10 @@
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  // console.error(err.stack);
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res
+      .status(400)
+      .json({ message: "Invalid JSON or empty request body." });
+  }
   res
     .status(500)
     .json({ message: "Internal Server Error", error: err.message });
